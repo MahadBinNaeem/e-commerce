8df220c8dfb694b0297792d85c914b8190dfc3bb
Mahad Bin Naeem <mahad.bin.naeem@xprolabs.com>
2023-10-23 15:11:57 +0500

    User: Password Validation and confirmable for email confirmation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validate :password_complexity
+
+  def password_complexity
+    if password.present? && !password.match(/^(?=.*[!@#\$%\^&*])[A-Za-z\d!@#\$%\^&*]{8,20}$/)
+      errors.add :password, "must contain at least one special character and be between 8 and 10 characters in length."
+    end
+  end
 end
