ddcb2bd7884f3e5cc958fbf67834725837961386
Mahad Bin Naeem <mahad.bin.naeem@xprolabs.com>
2023-10-26 19:30:37 +0500

    User: Association with role and user_role added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   PASSWORD_REGEX=/^(?=.*[!@#\$%\^&*])[A-Za-z\d!@#\$%\^&*]{8,20}$/
   validate :password_complexity
 
@@ -10,4 +13,9 @@ class User < ApplicationRecord
       errors.add :password, "must contain at least one special character and be between 8 and 10 characters in length."
     end
   end
+
+  def admin?
+    roles.exists?(name: 'admin')
+  end
+
 end
