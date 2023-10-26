1922ca5ab87c3b3ba6e9929a1683cdd96cfb6702
Mahad Bin Naeem <mahad.bin.naeem@xprolabs.com>
2023-10-26 12:06:38 +0500

    User: Fix repeated code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,15 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@user = User.all
 	end
 
-	def show
-		@user = User.find(params[:id])
-	end
+	def show;end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+	def edit;end
 
 	def update
-		@user = User.find(params[:id])
-
 		if @user.update(user_params)
 			redirect_to @user
 		else
@@ -23,7 +19,6 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-  	@user = User.find(params[:id])
     @user.destroy
     redirect_to users_path, notice: 'User was successfully deleted.'
   end
@@ -33,5 +28,9 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:name,:email,:password,:password_confirmation)
 	end
+
+	def set_user
+    @user = User.find(params[:id])
+  end
 
 end
