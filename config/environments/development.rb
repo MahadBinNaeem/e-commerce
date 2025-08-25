14171c3cde11380092aa1ee015fc8a8efcf207dc
Mahad Bin Naeem <mahad.bin.naeem@xprolabs.com>
2025-08-25 15:02:18 +0500

    Setup rspecs and fixed the configuration issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.smtp_settings = {
     address: '127.0.0.1',
     port: 1025  # MailCatcher's SMTP port
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
-  root to: 'products#index'
+  devise_for :users
+  root to: 'welcome#index'
   get '/users', to: 'welcome#index'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
